f6e0e01fcc5fcb79ed44ee669954e680c7400a9a
naeemmusamh <naeem@DESKTOP-RJ6QHBJ.localdomain>
2021-02-23 18:28:19 +0200

    new branch and new

--- JavaScript.js
+++ JavaScript.js
@@ -2,14 +2,20 @@
 
 let hightTryclick = 25;
 let startclick = 0;
+//push all the name to this array
 let newArray = [];
+//push all the name for this array
 let nameProdect = [];
+//push all the counter vote for this array
 let voteProduct = [];
+//push all the counter show for this array
 let showProduct = [];
+//global scope
 let image1Element = document.getElementById('image1');
 let image2Element = document.getElementById('image2');
 let image3Element = document.getElementById('image3');
 
+//an object
 function Mall(name, source) {
     this.name = name;
     this.source = source;
@@ -18,7 +24,7 @@ function Mall(name, source) {
     newArray.push(this);
     nameProdect.push(this.name);
 }
-
+//new object
 new Mall('bag', 'img/bag.jpg');
 new Mall('banana', 'img/banana.jpg');
 new Mall('bathroom', 'img/bathroom.jpg');
@@ -42,17 +48,17 @@ new Mall('wine-glass', 'img/wine-glass.jpg');
 
 // console.log(newArray);
 
+//global scope for the image i want to show in the website
 let image1Box;
 let image2Box;
 let image3Box;
-let imagecorrect = [];
-console.log(imagecorrect);
-
+//function it response for show the image in the website
 function renderThereimage() {
+    //make the image show randomly
     image1Box = randomNumber();
     image2Box = randomNumber();
     image3Box = randomNumber();
-
+    //condition for the image to not replay in the same time it show in the website
     do {
         image1Box = randomNumber();
         if (image1Box === image2Box) {
@@ -64,42 +70,39 @@ function renderThereimage() {
         }
     }
     while (image1Box === image2Box || image2Box === image3Box || image3Box === image1Box)
-
+    //make all the image declare in the element tag
     image1Element.setAttribute('src', newArray[image1Box].source);
     image2Element.setAttribute('src', newArray[image2Box].source);
     image3Element.setAttribute('src', newArray[image3Box].source);
     newArray[image2Box].show++;
     newArray[image1Box].show++;
     newArray[image3Box].show++;
-    // for (let x = 0; x < newArray.length; x++) {
-    //     imagecorrect.push(image1Element);
-    //     image2Element.push(image2Element);
-    //     imagecorrect.push(image3Element);
-    // }
 }
 renderThereimage();
 
+//function for calculate the random image to show
 function randomNumber() {
     let randomnumber = Math.floor(Math.random() * newArray.length);
     return randomnumber;
 }
 
-
+//declare the image to change in the click
 image1Element.addEventListener('click', handleClicking);
 image2Element.addEventListener('click', handleClicking);
 image3Element.addEventListener('click', handleClicking);
 
 
-
+//function to do calculate the vote number and to declare the event listener be click and to create element tag for unorderlist and li
 function handleClicking(event) {
     startclick++;
     if (startclick <= hightTryclick) {
-
+        //condition for the image click
         if (event.target.id === 'image1' || event.target.id === 'image2' || event.target.id === 'image3') {
             newArray[image1Box].vote++;
             newArray[image2Box].voe++;
             newArray[image3Box].vote++;
         }
+        addlocalStorage();
         renderThereimage();
     } else {
         let submitElement = document.getElementById('b1');
@@ -108,7 +111,7 @@ function handleClicking(event) {
         unorderListelement.addEventListener('click', unorderlist);
 
         function unorderlist(event) {
-
+            //for loop to push and create li inside the unorderlist
             for (let x = 0; x < newArray.length; x++) {
                 let liElement = document.createElement('li');
                 unorderListelement.appendChild(liElement);
@@ -117,22 +120,24 @@ function handleClicking(event) {
             }
 
         }
+        //for loop to push the vote counter and show counter to the array
         for (let x = 0; x < newArray.length; x++) {
             voteProduct.push(newArray[x].vote);
             showProduct.push(newArray[x].show);
         }
         chartform();
+        //to remove the replay in the bottom click
         image1Element.removeEventListener('click', handleClicking);
         image2Element.removeEventListener('click', handleClicking);
         image3Element.removeEventListener('click', handleClicking);
     }
 
 }
-
+//function to create the chart.js
 function chartform() {
     var ctx = document.getElementById('myChart').getContext('2d');
     var chart = new Chart(ctx, {
-        type: 'polarArea',
+        type: 'bar',
         data: {
             labels: nameProdect,
             datasets: [{
@@ -152,3 +157,23 @@ function chartform() {
 }
 // chartform();
 // console.log(chartform);
+
+
+function addlocalStorage() {
+    let data = JSON.stringify(newArray);
+    localStorage.setItem('AllOrders', data);
+}
+// console.log(addlocalStorage);
+
+function savaData() {
+    let saveDataitem = JSON.parse(localStorage.getItem('AllOrders'));
+    // console.log(saveDataitem);
+    if (saveDataitem) {
+        newArray = saveDataitem;
+    } else {
+        newArray = [];
+    }
+    renderThereimage();
+}
+// console.log(savaData());
+savaData();
